63f0d2bc0e40da0e8dba4be836797a3fcb845670
meswain1123 <meswain@gmail.com>
2020-08-25 16:50:07 -0600

    Hopefully ready for EB

---
+++ client/build/precache-manifest.783c547d18d40cb26075e08ad5739a43.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f218c8d9c4ca16894de72ced0b18bd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd5f995e052dbca60192",
+    "url": "/static/css/main.7e731367.chunk.css"
+  },
+  {
+    "revision": "b0768a7a430f61dada26",
+    "url": "/static/js/2.93d0c3d4.chunk.js"
+  },
+  {
+    "revision": "587d187ab46dadc20baeb9323d1f9a34",
+    "url": "/static/js/2.93d0c3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5f995e052dbca60192",
+    "url": "/static/js/main.684fa0c3.chunk.js"
+  },
+  {
+    "revision": "5e33f6aa1728acaf3c6e",
+    "url": "/static/js/runtime-main.608357f4.js"
+  },
+  {
+    "revision": "5bb802863600c653d2b8041daa75b3c8",
+    "url": "/static/media/AbandonedWarehouse.5bb80286.png"
+  },
+  {
+    "revision": "741a853e537af3bd2f88f1abbfbf2323",
+    "url": "/static/media/Bald.741a853e.png"
+  },
+  {
+    "revision": "94af444df7612c57f1db4848a9435c75",
+    "url": "/static/media/Bard.94af444d.png"
+  },
+  {
+    "revision": "a68280cb7a5e0028e00d335c6ea52abd",
+    "url": "/static/media/Basement-Study.a68280cb.jpg"
+  },
+  {
+    "revision": "4fcc70e3abad7ab07532d74906c765be",
+    "url": "/static/media/BlondeHair.4fcc70e3.png"
+  },
+  {
+    "revision": "9762d835f0ba8aeff6c85fa380320149",
+    "url": "/static/media/BlondeWoman.9762d835.png"
+  },
+  {
+    "revision": "df8f5004b61fa2366e8fa6571e9f732b",
+    "url": "/static/media/BlueSkin.df8f5004.png"
+  },
+  {
+    "revision": "2603a58af59db8bb963bd52b4112dd2c",
+    "url": "/static/media/BlueSkin2.2603a58a.png"
+  },
+  {
+    "revision": "4fa9735dc1ef59a1260acf1909102d7c",
+    "url": "/static/media/Castle-Wall.4fa9735d.jpg"
+  },
+  {
+    "revision": "e48ad0f2106119904a0aead3decc95e6",
+    "url": "/static/media/Cave-Lair.e48ad0f2.jpg"
+  },
+  {
+    "revision": "b70bce05cd97c9127d82fe3fbd1f9d25",
+    "url": "/static/media/Crossroads.b70bce05.jpg"
+  },
+  {
+    "revision": "469bcf6d2550816305049dbbd50f280f",
+    "url": "/static/media/DaggerAlley.469bcf6d.png"
+  },
+  {
+    "revision": "e48fb0f36aa1aa7bf683d2f9c0e3df0b",
+    "url": "/static/media/DarkSkin.e48fb0f3.png"
+  },
+  {
+    "revision": "ad2949fb274ff15352902c3a39aba456",
+    "url": "/static/media/DarkSkinF.ad2949fb.png"
+  },
+  {
+    "revision": "ab16b141af1cae763b39c7f797153e45",
+    "url": "/static/media/Dragonborn.ab16b141.png"
+  },
+  {
+    "revision": "c21c5f7d206497ec7ec48584c5edcc30",
+    "url": "/static/media/DragonbornFighter.c21c5f7d.png"
+  },
+  {
+    "revision": "2c31b739a8055334a9e45dcb7f609d9e",
+    "url": "/static/media/DwarfMale.2c31b739.png"
+  },
+  {
+    "revision": "4b6ed68831cdae649ab74109ae4e1c71",
+    "url": "/static/media/ElvenFemale.4b6ed688.png"
+  },
+  {
+    "revision": "855d86dece53315827406c62afa92043",
+    "url": "/static/media/ElvenFighter.855d86de.png"
+  },
+  {
+    "revision": "9e1fe3ec61a4579d50df14944345f512",
+    "url": "/static/media/Eyebrows.9e1fe3ec.png"
+  },
+  {
+    "revision": "e428b8ea7d220261448d41ce689c9f6b",
+    "url": "/static/media/Eyebrows2.e428b8ea.png"
+  },
+  {
+    "revision": "1802a7fa8801625aa2967281a52ea524",
+    "url": "/static/media/Farmstead.1802a7fa.jpg"
+  },
+  {
+    "revision": "ae0815147e8141f29a945baac95a483b",
+    "url": "/static/media/Fedora.ae081514.png"
+  },
+  {
+    "revision": "8706baa2ff4ff8a5d5e94624751daa72",
+    "url": "/static/media/Forest-Lair.8706baa2.jpg"
+  },
+  {
+    "revision": "0a1ba63d973b8057d4c288694292182f",
+    "url": "/static/media/GnomeMale.0a1ba63d.png"
+  },
+  {
+    "revision": "33cfc33ddbde26bd609b4b034803c879",
+    "url": "/static/media/GoliathBarb.33cfc33d.png"
+  },
+  {
+    "revision": "c1b9e380cf28a1f4a1d5513dd3adf33a",
+    "url": "/static/media/GreyHair.c1b9e380.png"
+  },
+  {
+    "revision": "639efdd9bda74769caff46b956583396",
+    "url": "/static/media/HalflingMale.639efdd9.png"
+  },
+  {
+    "revision": "9db2c27f26c73a8d603587bf5051fb6e",
+    "url": "/static/media/Hood.9db2c27f.png"
+  },
+  {
+    "revision": "70d96a277f78e5471a432ab8e217155f",
+    "url": "/static/media/Hood2.70d96a27.png"
+  },
+  {
+    "revision": "52eda96a466db1a8ae294f17bdbfd4f2",
+    "url": "/static/media/Human.52eda96a.png"
+  },
+  {
+    "revision": "81c31b30925d26985dcdee5e5bacd4a3",
+    "url": "/static/media/HumanFemale.81c31b30.png"
+  },
+  {
+    "revision": "a4ff5aca98c2914c4a5e3a2c784badc4",
+    "url": "/static/media/HumanFemale2.a4ff5aca.png"
+  },
+  {
+    "revision": "533cf974b8706fdea090077f0d7d5e8f",
+    "url": "/static/media/HumanFighterF.533cf974.png"
+  },
+  {
+    "revision": "77a0b7de1b889bf85ef93e7419aadb1a",
+    "url": "/static/media/HumanFighterM.77a0b7de.png"
+  },
+  {
+    "revision": "69efd308adb017b8970e59d10b116bca",
+    "url": "/static/media/HumanMale.69efd308.png"
+  },
+  {
+    "revision": "19c85bbcac8ffa907429ad9305c243eb",
+    "url": "/static/media/HumanMale2.19c85bbc.png"
+  },
+  {
+    "revision": "7d208560e5b3624a27b689bbb1589f10",
+    "url": "/static/media/Knight.7d208560.png"
+  },
+  {
+    "revision": "906aa148575536397217a20b8b556b7c",
+    "url": "/static/media/Monster-Lair.906aa148.jpg"
+  },
+  {
+    "revision": "b670e555c87f2082c9a61cd29b5ff7c2",
+    "url": "/static/media/OldWoman.b670e555.png"
+  },
+  {
+    "revision": "28ceea2675ce4f7468fef91897a58d35",
+    "url": "/static/media/OldWoman2.28ceea26.png"
+  },
+  {
+    "revision": "9c811852290983c18b42e886ed15e8d7",
+    "url": "/static/media/PitTrapWSpikes.9c811852.png"
+  },
+  {
+    "revision": "7b7286083f27dccf4145b157b43dc29a",
+    "url": "/static/media/Rat.7b728608.png"
+  },
+  {
+    "revision": "75d09fc644ca1334e4cedc16fcf746be",
+    "url": "/static/media/RedHair.75d09fc6.png"
+  },
+  {
+    "revision": "bd87a6126b370580a2126ba9af242bde",
+    "url": "/static/media/RedSkin.bd87a612.png"
+  },
+  {
+    "revision": "39f39c68c53b96601d62bc491e6b7282",
+    "url": "/static/media/RugOfSmothering.39f39c68.jpeg"
+  },
+  {
+    "revision": "cbbf87ae92014bed640e58525cd08b27",
+    "url": "/static/media/Samurai.cbbf87ae.png"
+  },
+  {
+    "revision": "e6d10ad610ef364e8b5e61bb7ce2bb72",
+    "url": "/static/media/Scarf.e6d10ad6.png"
+  },
+  {
+    "revision": "e08e1847b320d05d9ca6178e19421adb",
+    "url": "/static/media/Snow-Village.e08e1847.jpg"
+  },
+  {
+    "revision": "625e577e7c5a160192bb45487bbf3425",
+    "url": "/static/media/Swamp-Lair.625e577e.jpg"
+  },
+  {
+    "revision": "9c60ea48b8d1e6f9953aff20dc6d2a09",
+    "url": "/static/media/Tavern.9c60ea48.jpg"
+  },
+  {
+    "revision": "1ee22c7cd9c7b41d7a5e46c7501367f6",
+    "url": "/static/media/TieflingFemale.1ee22c7c.png"
+  },
+  {
+    "revision": "a7ce5ec0f7a2f6bc240139a9956c0793",
+    "url": "/static/media/TrollSkullAlleyBasement.a7ce5ec0.png"
+  },
+  {
+    "revision": "9a7bd522667950b5f94c9bfe0241baca",
+    "url": "/static/media/TrollSkullAlleyHouse.9a7bd522.png"
+  },
+  {
+    "revision": "4588f8f4adff585e5bb791bbfc4d6b17",
+    "url": "/static/media/UnderDaggerAlley.4588f8f4.png"
+  },
+  {
+    "revision": "ef43ba2b9623f74eecea239bb19154a6",
+    "url": "/static/media/Underground-Complex.ef43ba2b.jpg"
+  },
+  {
+    "revision": "f9494f9de0acadcfed0c3f03f95d4853",
+    "url": "/static/media/Urban-Lair.f9494f9d.jpg"
+  },
+  {
+    "revision": "ad53d0a13d167637adc0c0ceabc317a1",
+    "url": "/static/media/Wagon-Trail-and-Shrine.ad53d0a1.jpg"
+  },
+  {
+    "revision": "8d0ffe050300e70185431aabfc22afe7",
+    "url": "/static/media/Waterfall-Cavern.8d0ffe05.jpg"
+  },
+  {
+    "revision": "cade6e88cd0ef9fb151767cecff0284f",
+    "url": "/static/media/WeddingRing.cade6e88.png"
+  },
+  {
+    "revision": "d2f362991566d0ccc8cfb2cfc525d810",
+    "url": "/static/media/Wererat.d2f36299.png"
+  },
+  {
+    "revision": "17e32e138535401b117a6f8abeb5a09e",
+    "url": "/static/media/Werewolf.17e32e13.png"
+  },
+  {
+    "revision": "6cbb39024a0673c989c36fcceef201be",
+    "url": "/static/media/Wizard.6cbb3902.png"
+  },
+  {
+    "revision": "8cdd4c945e349191a975adc1d377cb0b",
+    "url": "/static/media/idk.8cdd4c94.png"
+  }
+]);
